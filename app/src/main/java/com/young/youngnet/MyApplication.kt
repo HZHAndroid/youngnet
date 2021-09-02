3960dad5392de3a4101054cb30d7ec58bc7598a5
蓝月羽 <1104090872@qq.com>
2021-09-02 08:33:09 +0800

    add: 1、新增了网络请求的封装； 2、在 app 模块新建了 application； 3、net 模块假如了 retrofit 和 okhttp 的混淆配置； 4、开放出 retrofit api，否则提供出的 TokenInterceptor 会无法访问到 Interceptor 类； 5、开放出 gson ，也是为了外部使用；

--- app/src/main/java/com/young/youngnet/MyApplication.kt
+++ app/src/main/java/com/young/youngnet/MyApplication.kt
@@ -1,6 +1,14 @@
 package com.young.youngnet
 
 import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import com.young.net.NetInit
+import com.young.net.callback.ICommonErrorCallback
+import com.young.net.callback.IDoNetConfig
+import com.young.net.constant.ErrorCode
+import com.young.net.exception.ApiException
+import com.young.net.init.NetConfig
 
 /**
  * @author:  Young
@@ -28,6 +36,60 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initNet()
+    }
+
+    /**
+     * 初始化网络
+     */
+    private fun initNet() {
+        NetInit.setBaseUrl("http://rap2api.taobao.org/app/mock/289099/") // 设置基础的请求域名，需要一 / 结尾
+            .setCommonErrorCallback(object : ICommonErrorCallback {
+                override fun onCall(e: ApiException) {
+                    // 只要请求异常，都会调用当前方法
+                    // 假如需要特殊处理，假如国际化，或者想变换一下提示消息，可以进行下面的判断，然后利用自定义的消息
+                    // 进行提示，假如不需要，可以直接使用 e.msg
+//                    when (e.code) {
+//                        ErrorCode.NETWORK_ERROR -> {
+//                            // 网络请求异常
+//                        }
+//                        ErrorCode.PARSE_ERROR -> {
+//                            // 数据解析异常
+//                        }
+//                        ErrorCode.DOWNLOAD_EMPTY -> {
+//                            // 下载的内容为空
+//                        }
+//                        ErrorCode.UNKNOWN_ERROR -> {
+//                            // 其他异常
+//                        }
+//                    }
+                    showToast(e.msg ?: "")
+                }
+            })
+            .init(object : IDoNetConfig {
+
+                override fun onConfig(config: NetConfig) {
+                    // 普通 api 请求的配置
+//                    config
+//                        .addInterceptor(TestTokenInterceptor())
+//                        .addNetworkInterceptor(
+//                            HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+//                                override fun log(message: String) {
+//                                    Log.e("shenlong", "message = $message")
+//                                }
+//                            }).setLevel(HttpLoggingInterceptor.Level.BODY)
+//                        )
+                }
+
+            }, object : IDoNetConfig {
+                override fun onConfig(config: NetConfig) {
+                    // 上传下载 api 请求的配置
+                }
+            })
+    }
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 
 }
---
+++ app/src/main/java/com/young/youngnet/interceptor/TestTokenInterceptor.kt
@@ -0,0 +1,27 @@
+package com.young.youngnet.interceptor
+
+/**
+ * @author:  Young
+ * .
+ * @email:   1104090872@qq.com
+ * .
+ * @date :   2021/8/29 17:43 周日
+ * 　　 へ　　　　　／|
+　　/＼7　　∠＿/
+　 /　│　　 ／　／
+　│　Z ＿,＜　／　　 /`ヽ
+　│　　　　　ヽ　　 /　　〉
+　 Y　　　　　`　 /　　/
+　ｲ●　､　●　　⊂⊃〈　　/
+　()　 へ　　　　|　＼〈
+　　>ｰ ､_　 ィ　 │ ／／
+　 / へ　　 /　ﾉ＜| ＼＼
+　 ヽ_ﾉ　　(_／　 │／／
+　　7　　　　　　　|／
+　　＞―r￣￣`ｰ―＿
+
+ * @description:
+ */
+class TestTokenInterceptor {
+
+}
