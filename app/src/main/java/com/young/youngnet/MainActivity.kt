71650fba80f4c99cbdc5a7821c626672c16f265a
蓝月羽 <1104090872@qq.com>
2021-09-02 22:27:13 +0800

    add: 1、新增了网络请求的封装； 2、在 app 模块新建了 application； 3、net 模块假如了 retrofit 和 okhttp 的混淆配置； 4、开放出 retrofit api，否则提供出的 TokenInterceptor 会无法访问到 Interceptor 类； 5、开放出 gson ，也是为了外部使用； 6、加入了 retrofit 的 Call<?> 回调给调用者，方便去做关闭等操作； 7、加入了日志拦截器的类，方便有直接转换成 body 的拓展；

--- app/src/main/java/com/young/youngnet/MainActivity.kt
+++ app/src/main/java/com/young/youngnet/MainActivity.kt
@@ -1,38 +1,32 @@
 package com.young.youngnet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.young.net.YoungNetWorking
-import com.young.net.callback.ICallback
-import com.young.net.callback.IGetCall
-import com.young.net.exception.ApiException
-import retrofit2.Call
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        YoungNetWorking.createBodyClientCreator("user/list", Any::class.java)
-            .addParam("hello", "你好")
-            .addHeader("token", "tokjskjdf")
-            .addHeader("shenxian", "i am power")
-            .setGetCall(object :IGetCall {
-                override fun onGet(call: Call<*>) {
-                    Log.e("shenlong","call call call ${call}")
-                }
-
-            })
-            .build()
-            .post(object : ICallback<Any> {
-                override fun onFailure(e: ApiException) {
-                    Log.e("shenlong", "kk onFailure = $e")
-                }
-
-                override fun onSuccess(data: Any?) {
-                    Log.e("shenlong", "kk tset = " + data + " " + Thread.currentThread().name)
-                }
-            })
+//        YoungNetWorking.createBodyClientCreator("user/list", Any::class.java)
+//            .addParam("hello", "你好")
+//            .addHeader("token", "tokjskjdf")
+//            .addHeader("shenxian", "i am power")
+//            .setGetCall(object :IGetCall {
+//                override fun onGet(call: Call<*>) {
+//                    Log.e("shenlong","call call call ${call}")
+//                }
+//
+//            })
+//            .build()
+//            .post(object : ICallback<Any> {
+//                override fun onFailure(e: ApiException) {
+//                    Log.e("shenlong", "kk onFailure = $e")
+//                }
+//
+//                override fun onSuccess(data: Any?) {
+//                    Log.e("shenlong", "kk tset = " + data + " " + Thread.currentThread().name)
+//                }
+//            })
     }
 }
